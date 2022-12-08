47749763da58cb076f4226cd16f1830d2ce4d341
hasirciogli <unconfigured@null.spigotmc.org>
2022-12-08 18:49:40 +0300

    supress 2

--- imgui-menu-design/menu/Menu.cpp
+++ imgui-menu-design/menu/Menu.cpp
@@ -197,7 +197,7 @@ void Menu::runCustomGui(LPDIRECT3DDEVICE9 pDevice, bool param) {
 			{
 				ImVec2 mPos = {};
 
-				getMenuPos(mPos);
+				getMenuPos(mPos); 
 
 				ImGui::GetWindowDrawList()->AddCircleFilled(ImVec2(mPos.x + calculateUiScalar((220 / 3) / 2), mPos.y + calculateUiScalar(65 / 2)), 20, ImColor(140, 140, 140));
 				imspaceMacro(calculateUiScalar((220 / 3 + 10)), calculateUiScalar(65 / 2) - (ImGui::CalcTextSize("Hasirciogli", nullptr).y / 2));
